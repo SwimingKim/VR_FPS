d47ad0c9abfc4f48f8a0f7185a9ba7897bb8a570
KimSuyoung <rumex13@gmail.com>
2018-04-04 19:08:35 +0900

    초기 셋팅

---
+++ Assets/Scripts/CCharacterAnimation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCharacterAnimation : MonoBehaviour {
+
+	public enum ANIM_TYPE
+	{
+		IDLE, WALK, JUMP, ATTACK, DIE 
+	}
+	public ANIM_TYPE _animType = ANIM_TYPE.IDLE;
+
+	private Animator _animator;
+	public float _animSpeed;
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_animator.speed = _animSpeed;
+	}
+
+	public void PlayAnimation(ANIM_TYPE animType)
+	{
+		_animType = animType;
+
+		switch (animType)
+		{
+			case ANIM_TYPE.IDLE:
+				_animator.SetBool("Move", false);
+				break;
+			case ANIM_TYPE.WALK:
+				_animator.SetBool("Move", true);
+				break;
+			case ANIM_TYPE.JUMP:
+				_animator.SetTrigger("Jump");
+				break;
+			case ANIM_TYPE.ATTACK:
+				_animator.SetBool("Animing", true);
+				_animator.SetTrigger("Attack");
+				break;
+		}
+	}
+
+	public void setSpeedValue(float value)
+	{
+		_animator.SetFloat("Speed", value);
+	}
+
+	public float getSpeedValue()
+	{
+		return _animator.GetFloat("Speed");
+	}
+
+}
---
+++ Assets/Scripts/CInputMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CInputMovement : MonoBehaviour
+{
+
+    CharacterController _cc;
+	CCharacterAnimation _anim;
+
+    Vector3 _moveDir = Vector3.zero;
+
+    public float _speed;
+    public float _gravity;
+
+    void Awake()
+    {
+        _cc = GetComponent<CharacterController>();
+		_anim = GetComponent<CCharacterAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		_moveDir = new Vector3(h, 0f, v);
+		if (_moveDir == Vector3.zero)
+		{
+			_anim.setSpeedValue(0f);
+			// _anim.PlayAnimation(CCharacterAnimation.ANIM_TYPE.IDLE);
+		}
+		else
+		{
+			transform.forward = _moveDir.normalized;
+
+			float speed = _speed;
+			if (h != 0 && v != 0)
+			{
+				float degree = Mathf.Cos(45f * Mathf.Deg2Rad);
+				speed *= degree;
+			}
+			
+			_moveDir *= speed;
+			_moveDir.y -= _gravity;
+
+			_anim.setSpeedValue(0.4f);
+			// _anim.PlayAnimation(CCharacterAnimation.ANIM_TYPE.WALK);
+			_cc.Move(_moveDir * Time.deltaTime);
+
+			return;
+		}
+    }
+
+}
