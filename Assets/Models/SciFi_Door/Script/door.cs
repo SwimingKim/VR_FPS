511a64fdcd028e2ec2e9ee964c3b65fa36994aef
KimSuyoung <rumex13@gmail.com>
2018-04-06 16:19:18 +0900

    add door asset

---
+++ Assets/Models/SciFi_Door/Script/door.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class door : MonoBehaviour {
+	GameObject thedoor;
+
+void OnTriggerEnter ( Collider obj  ){
+	thedoor= GameObject.FindWithTag("SF_Door");
+	thedoor.GetComponent<Animation>().Play("open");
+}
+
+void OnTriggerExit ( Collider obj  ){
+	thedoor= GameObject.FindWithTag("SF_Door");
+	thedoor.GetComponent<Animation>().Play("close");
+}
+
+void OnCollisionEnter(Collision collisionInfo)
+{	
+	Debug.Log("!!");
+	
+}
+}
