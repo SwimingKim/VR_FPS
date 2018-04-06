9200b292de09c2a6c95372407fae201e4d2dd649
KimSuyoung <rumex13@gmail.com>
2018-04-06 23:57:17 +0900

    setting font

--- Assets/Scripts/CGameManager.cs
+++ Assets/Scripts/CGameManager.cs
@@ -1,19 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CGameManager : MonoBehaviour {
+public class CGameManager : MonoBehaviour
+{
+    public Text _timerText;
+    public Text _messageText;
+	public Animation[] _anims;
 
-	[SerializeField] CCameraManager cameraManager;
-	[SerializeField] CCharacterManager characterManager;
+    [SerializeField] CCameraManager cameraManager;
+    [SerializeField] CCharacterManager characterManager;
 
-	// Update is called once per frame
-	void Update () {
+	void Start()
+	{
+		Debug.Log(PhotonNetwork.room.PlayerCount.ToString());
+		
+		StartCoroutine("ShowTimer");
+	}
 
-		if (Input.GetKeyDown(KeyCode.W))
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            cameraManager.IsRun = !cameraManager.IsRun;
+            characterManager._anim.PlayAnimation(cameraManager.IsRun ? CCharacterAnimation.ANIM_TYPE.WALK : CCharacterAnimation.ANIM_TYPE.IDLE);
+        }
+    }
+
+	IEnumerator ShowTimer()
+	{
+		yield return new WaitForSeconds(1);
+		int sec = int.Parse(_timerText.text);
+		if (sec > 0)
 		{
-			cameraManager.IsRun = !cameraManager.IsRun;
-			characterManager._anim.PlayAnimation( cameraManager.IsRun ? CCharacterAnimation.ANIM_TYPE.WALK : CCharacterAnimation.ANIM_TYPE.IDLE );
+			_timerText.text = (sec-1).ToString();
+			StartCoroutine("ShowTimer");
+		}
+		else
+		{
+			_timerText.text = "Good Luck, "+PhotonNetwork.playerName;
+			_messageText.text = "Time to Attack";
+			for (var i=0; i < _anims.Length; i++)
+			{
+				_anims[i].Play("open");
+			}
+			StopCoroutine("ShowTimer");
 		}
 		
 	}
