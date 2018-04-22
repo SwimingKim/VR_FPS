1f81498d09201533e9dfe02e9364bb43dfbafb36
skim <rumex13@naver.com>
2018-04-23 02:12:55 +0900

    canvas 유동적 변경

---
+++ Assets/Scripts/CCanvasSize.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCanvasSize : MonoBehaviour {
+
+	void Start()
+	{
+		RectTransform rt = GetComponent<RectTransform>();
+		#if UNITY_ANDROID
+		rt.sizeDelta = new Vector2(1920, 1450);
+		#elif UNITY_EDITOR || UNITY_WEBGL
+		rt.sizeDelta = new Vector2(1920, 1080);
+		#endif		
+		Debug.Log(rt.rect.height);
+
+	}
+
+}
